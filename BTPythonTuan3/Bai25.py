97eb822e486439b8614a257cf9d1000230bf917c
TuyetBangTK01 <49788998+TuyetBangTK01@users.noreply.github.com>
2019-07-07 12:35:51 +0700

    BTPythonT3

---
+++ BTPythonTuan3/Bai25.py
@@ -0,0 +1,9 @@
+freq = {} # frequency of words in text
+line = input()
+for word in line.split():
+ freq[word] = freq.get(word,0)+1
+
+words = sorted(freq.keys())
+
+for w in words:
+    print ("%s:%d" % (w,freq[w]))
